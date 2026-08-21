17ed17c059726ae53daa6dec98e4b4c80346f27c
Karl Stock <karlst@soloran.com>
2026-08-21 13:36:54 -0700

    Fix regression - remove call to cleanup() in buffer_manager.

--- video_capture/buffer_manager.py
+++ video_capture/buffer_manager.py
@@ -248,7 +248,6 @@ class BufferManager:
                         summary="Sidecar analysis failed"
                     )
 
-            self._capture_manager.cleanup()
 
         capture_status = {
             "buffer_count": len(
